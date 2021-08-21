0f22ca14cd412512ad93d9cb07fa34447e001dfd
Giorgia Campardo <giorgiac98.gc@gmail.com>
2021-08-21 15:22:38 +0200

    something works

--- CP/src/launch.py
+++ CP/src/launch.py
@@ -2,10 +2,29 @@ from minizinc import Instance, Model, Solver
 import matplotlib.pyplot as plt
 from matplotlib.patches import Rectangle
 
+def solve_model(w, c, dim):
+    # Load the model from file
+    model = Model("vlsi.mzn")
+    # Find the MiniZinc solver configuration for Gecode
+    gecode = Solver.lookup("gecode")
+    # Create an Instance of the model for Gecode
+    instance = Instance(gecode, model)
+    # Assign 4 to n
+    instance["w"] = w
+    instance['c'] = c
+    instance['dx'] = [x[0] for x in dim]
+    instance['dy'] = [y[0] for y in dim]
+
+    result = instance.solve()
+    return instance, result
+
 def plot(width, height, blocks):
+    colors = ['red', 'blue', 'green', 'cyan', 'pink', 'purple', 'brown', 'olive', 'grey', 'orange']
+
     fig, ax = plt.subplots()
-    for w,h,x,y,c in blocks:
-        ax.add_patch(Rectangle((x,y), w, h, facecolor=c, lw=1))
+    for i in range(0, len(blocks)):
+        w,h,x,y = blocks[i]
+        ax.add_patch(Rectangle((x,y), w, h, facecolor=colors[i], lw=1))
 
     ax.set_ylim(0, height)
     ax.set_xlim(0, width)
@@ -14,26 +33,25 @@ def plot(width, height, blocks):
     ax.grid(True)
     plt.show()
 
-# Load n-Queens model from file
-nqueens = Model("vlsi.mzn")
-# Find the MiniZinc solver configuration for Gecode
-gecode = Solver.lookup("gecode")
-# Create an Instance of the n-Queens model for Gecode
-instance = Instance(gecode, nqueens)
-# Assign 4 to n
-instance["w"] = 9
-instance['c'] = 5
-instance['dx'] = [3, 2, 2, 3, 4]
-instance['dy'] = [3, 4, 8, 9, 12]
 
-result = instance.solve()
-# Output
-print(result)
-print(result["h"])
-print(result["x"])
-print(result["y"])
+for i in range(1,41):
+    lines = []
+    with open(f'../../instances/ins-{i}.txt') as f:
+        lines = f.readlines()
+    lines = [l.strip('\n') for l in lines]
+    dim = [l.split(' ') for l in lines[2:]]
+    dim = [[int(d[0]), int(d[1])] for d in dim]
+    w = int(lines[0].strip('\n'))
+    c = int(lines[1].strip('\n'))
 
-colors = ['red', 'blue', 'green', 'cyan', 'pink']
+    instance, result = solve_model(w, c, dim)
+    # Output
+    print(result)
+    print(result["h"])
+    print(result["x"])
+    print(result["y"])
 
-plot(9, result["h"], [(instance['dx'][i], instance['dy'][i], result["x"][i], result["y"][i], colors[i]) for i in range(0,5)])
-# [(3,3,4,0,'red'), (2,4,7,0,'blue'), (2,8,7,4,'green'), (3,9,4,3,'cyan'), (4,12,0,0,'pink')])
+    #with open(f'out/ins-{i}.txt') as f:
+
+
+    plot(w, result["h"], [(instance['dx'][i], instance['dy'][i], result["x"][i], result["y"][i]) for i in range(0,c)])
