a9bcf17a9bc743d15409dd931093820d87d7345c
diegochine <chine.diego@gmail.com>
2021-08-30 00:00:11 +0200

    fixed implied constraints

--- SMT/src/base_model.py
+++ SMT/src/base_model.py
@@ -6,17 +6,24 @@ def base_model(instance):
     # define main problem variables and constraints
     w, l = Ints('width length')
     components = list(range(instance['n']))
-
+    # board for dual model
+    board = [[Int(f'B_{c}_{r}') for r in range(instance["w"])] for c in range(instance['maxl'])]
     vs['w'], vs['l'] = w, l
+    vs['board'] = board
     # basic problem constraints
     constraints = [vs['w'] == instance["w"], vs['l'] >= instance['minl'], vs['l'] <= instance['maxl']]
 
     for i in components:
         x_i, y_i = Ints(f'x_{i} y_{i}')
+        xhat_i, yhat_i = Ints(f'xhat_{i} yhat_{i}')
+        area_i = Int(f'area_{i}')
+        vs[f'x_{i}'], vs[f'y_{i}'] = x_i, y_i
+        vs[f'xhat_{i}'], vs[f'yhat_{i}'] = xhat_i, yhat_i
+        vs[f'area_{i}'] = area_i
+
         constraints.append(x_i == (instance['inputx'][i]))
         constraints.append(y_i == (instance['inputy'][i]))
 
-        xhat_i, yhat_i = Ints(f'xhat_{i} yhat_{i}')
         constraints.append(xhat_i >= 0)
         constraints.append(xhat_i < w)
         constraints.append(xhat_i + x_i <= w)
@@ -24,18 +31,19 @@ def base_model(instance):
         constraints.append(yhat_i < l)
         constraints.append(yhat_i + y_i <= l)
 
-        area_i = Int(f'area_{i}')
         constraints.append(area_i == (x_i * y_i))
 
-        vs[f'x_{i}'], vs[f'y_{i}'] = x_i, y_i
-        vs[f'xhat_{i}'], vs[f'yhat_{i}'] = xhat_i, yhat_i
-        vs[f'area_{i}'] = area_i
 
-        # implied constraints: for each horizontal (and vertical) line, the sum of traversed sides is bounded
-        constraints += [Sum([If(And(vs[f'xhat_{i}'] < col, col <= vs[f'xhat_{i}'] + vs[f'x_{i}']), vs[f'y_{i}'], 0)])
-                        <= vs['w'] for col in range(instance['w'])]
-        constraints += [Sum([If(And(vs[f'yhat_{i}'] < row, row <= vs[f'yhat_{i}'] + vs[f'y_{i}']), vs[f'x_{i}'], 0)])
-                        <= vs['l'] for row in range(instance['maxl'])]
+
+
+    # implied constraints: for each horizontal (and vertical) line, the sum of traversed sides is bounded
+    for col in range(instance['w']):
+        constraints.append(Sum([If(And(vs[f'xhat_{i}'] < col, col <= vs[f'xhat_{i}'] + vs[f'x_{i}']), vs[f'y_{i}'], 0)
+                                for i in components]) <= vs['w'])
+    for row in range(instance['maxl']):
+        constraints.append(Sum([If(And(vs[f'yhat_{i}'] < row, row <= vs[f'yhat_{i}'] + vs[f'y_{i}']), vs[f'x_{i}'], 0)
+                                for i in components]) <= vs['l'])
+
     # no overlap constraints
     constraints += [
         Or(vs[f'xhat_{i}'] + vs[f'x_{i}'] <= vs[f'xhat_{j}'], vs[f'yhat_{i}'] + vs[f'y_{i}'] <= vs[f'yhat_{j}'],
