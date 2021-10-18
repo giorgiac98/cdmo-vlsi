2b50b5dcc4348a1de94477f566b2039b9ec783c4
Giorgia Campardo <giorgiac98.gc@gmail.com>
2021-10-18 23:23:41 +0200

    commented dual model, added rotation

--- SMT/src/base_model.py
+++ SMT/src/base_model.py
@@ -18,15 +18,15 @@ def flatten(matrix):
     return [cell for row in matrix for cell in row]
 
 
-def base_model(instance):
+def base_model(instance, rotation):
     vs = {}
     # define main problem variables and constraints
     w, l = Ints('width length')
     components = list(range(instance['n']))
     # board for dual model
-    board = [[Int(f'B_{c}_{r}') for r in range(instance["w"])] for c in range(instance['maxl'])]
+    #board = [[Int(f'B_{c}_{r}') for r in range(instance["w"])] for c in range(instance['maxl'])]
     vs['w'], vs['l'] = w, l
-    vs['board'] = board
+    #vs['board'] = board
     # basic problem constraints
     constraints = [vs['w'] == instance["w"], vs['l'] >= instance['minl'], vs['l'] <= instance['maxl']]
 
@@ -38,8 +38,14 @@ def base_model(instance):
         vs[f'xhat_{i}'], vs[f'yhat_{i}'] = xhat_i, yhat_i
         vs[f'area_{i}'] = area_i
 
-        constraints.append(x_i == (instance['inputx'][i]))
-        constraints.append(y_i == (instance['inputy'][i]))
+        if rotation:
+            rotation_i = Bool(f'rotation_{i}')
+            vs[f'rotation_{i}'] = rotation_i
+            constraints.append(If(rotation_i, x_i == (instance['inputy'][i]), x_i == (instance['inputx'][i])))
+            constraints.append(If(rotation_i, y_i == (instance['inputx'][i]), y_i == (instance['inputy'][i])))
+        else:
+            constraints.append(x_i == (instance['inputx'][i]))
+            constraints.append(y_i == (instance['inputy'][i]))
 
         constraints.append(xhat_i >= 0)
         constraints.append(xhat_i < w)
@@ -51,14 +57,14 @@ def base_model(instance):
         constraints.append(area_i == (x_i * y_i))
 
         # channeling constraints
-        constraints += [(board[c][r] == i) == (And(vs[f'xhat_{i}'] <= r, vs[f'xhat_{i}'] + vs[f'x_{i}'] > r,
-                                                   vs[f'yhat_{i}'] <= c, vs[f'yhat_{i}'] + vs[f'y_{i}'] > c))
-                        for r in range(instance["w"]) for c in range(instance['maxl'])]
+        #constraints += [(board[c][r] == i) == (And(vs[f'xhat_{i}'] <= r, vs[f'xhat_{i}'] + vs[f'x_{i}'] > r,
+        #                                           vs[f'yhat_{i}'] <= c, vs[f'yhat_{i}'] + vs[f'y_{i}'] > c))
+        #                for r in range(instance["w"]) for c in range(instance['maxl'])]
 
         # implied constraints: for each component i, # of cells of the board with value i must be equal to area_i
-        constraints.append(Sum([If(board[c][r] == i, 1, 0)
-                                for r in range(instance["w"]) for c in range(instance['maxl'])])
-                           == area_i)
+        #constraints.append(Sum([If(board[c][r] == i, 1, 0)
+        #                        for r in range(instance["w"]) for c in range(instance['maxl'])])
+        #                   == area_i)
 
     # implied constraints: for each horizontal (and vertical) line, the sum of traversed sides is bounded
     for col in range(instance['w']):
@@ -69,8 +75,8 @@ def base_model(instance):
                                 for i in components]) <= vs['w'])
 
     # board constraints
-    constraints += [And(board[c][r] >= 0, board[c][r] <= instance['n'])
-                    for r in range(instance["w"]) for c in range(instance['maxl'])]
+    #constraints += [And(board[c][r] >= 0, board[c][r] <= instance['n'])
+    #                for r in range(instance["w"]) for c in range(instance['maxl'])]
 
     # no overlap constraints
     constraints += [
@@ -78,20 +84,35 @@ def base_model(instance):
            vs[f'xhat_{j}'] + vs[f'x_{j}'] <= vs[f'xhat_{i}'], vs[f'yhat_{j}'] + vs[f'y_{j}'] <= vs[f'yhat_{i}'])
         for i in components for j in components if i < j]
 
-    # symmetry breaking?
+    # symmetry breaking
+    # rows and columns symmetry
     constraints += [Implies(And(vs[f'xhat_{i}'] == vs[f'xhat_{j}'], vs[f'x_{i}'] == vs[f'x_{j}']),
-                            vs[f'yhat_{i}'] <= vs[f'yhat_{j}'])
+                            If(vs[f'y_{i}'] <= vs[f'y_{j}'], vs[f'yhat_{j}'] <= vs[f'yhat_{i}'], vs[f'yhat_{i}'] <= vs[f'yhat_{j}']))
                     for i in components for j in components if i < j]
 
     constraints += [Implies(And(vs[f'yhat_{i}'] == vs[f'yhat_{j}'], vs[f'y_{i}'] == vs[f'y_{j}']),
-                            vs[f'xhat_{i}'] <= vs[f'xhat_{j}'])
+                            If(vs[f'x_{i}'] <= vs[f'x_{j}'], vs[f'xhat_{j}'] <= vs[f'xhat_{i}'], vs[f'xhat_{i}'] <= vs[f'xhat_{j}']))
                     for i in components for j in components if i < j]
+    # three block symmetry
+    constraints += [Implies(And(vs[f'xhat_{i}'] == vs[f'xhat_{j}'], vs[f'x_{i}'] == vs[f'x_{j}'],
+                                vs[f'yhat_{i}'] == vs[f'yhat_{k}'], vs[f'y_{i}'] + vs[f'y_{j}'] == vs[f'y_{k}']),
+                            If(vs[f'x_{i}'] <= vs[f'x_{k}'],
+                                And(vs[f'xhat_{i}'] <= vs[f'xhat_{k}'], vs[f'xhat_{i}'] == vs[f'xhat_{j}']),
+                                And(vs[f'xhat_{k}'] <= vs[f'xhat_{i}'], vs[f'xhat_{i}'] == vs[f'xhat_{j}'])))
+                    for i in components for j in components for k in components if i < j and j < k]
 
-    constraints.append(lex(flatten(board),
-                           [board[j][i] for i in range(instance['w']) for j in range(instance['maxl'] - 1, -1, -1)]))
-    constraints.append(lex(flatten(board),
-                           [board[j][i] for i in range(instance['w'] - 1, -1, -1) for j in range(instance['maxl'])]))
-    constraints.append(lex(flatten(board),
-                           [board[j][i] for i in range(instance['w'] - 1, -1, -1) for j in range(instance['maxl'] - 1, -1, -1)]))
+    constraints += [Implies(And(vs[f'yhat_{i}'] == vs[f'yhat_{j}'], vs[f'y_{i}'] == vs[f'y_{j}'],
+                                vs[f'xhat_{i}'] == vs[f'xhat_{k}'], vs[f'x_{i}'] + vs[f'x_{j}'] == vs[f'x_{k}']),
+                            If(vs[f'y_{i}'] <= vs[f'y_{k}'],
+                                And(vs[f'yhat_{i}'] <= vs[f'yhat_{k}'], vs[f'yhat_{i}'] == vs[f'yhat_{j}']),
+                                And(vs[f'yhat_{k}'] <= vs[f'yhat_{i}'], vs[f'yhat_{i}'] == vs[f'yhat_{j}'])))
+                    for i in components for j in components for k in components if i < j and j < k]
+
+    #constraints.append(lex(flatten(board),
+    #                       [board[j][i] for i in range(instance['w']) for j in range(instance['maxl'] - 1, -1, -1)]))
+    #constraints.append(lex(flatten(board),
+    #                       [board[j][i] for i in range(instance['w'] - 1, -1, -1) for j in range(instance['maxl'])]))
+    #constraints.append(lex(flatten(board),
+    #                       [board[j][i] for i in range(instance['w'] - 1, -1, -1) for j in range(instance['maxl'] - 1, -1, -1)]))
 
     return constraints, vs
